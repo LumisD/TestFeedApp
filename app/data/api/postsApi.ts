1a3fd958ce2ebb1360a651d25d05ed9db347e818
alexSimch <alexey@xealenergy.com>
2025-07-25 11:07:51 -0400

    Delete postApi and PostRepository

--- app/data/api/postsApi.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { Post } from "../../domain/models/Post";
-
-const API_URL = "https://662029f13bf790e070af2cd8.mockapi.io/api/v1/posts";
-
-export async function fetchPosts(): Promise<Post[]> {
-  const response = await axios.get<Post[]>(API_URL);
-  return response.data;
-}
