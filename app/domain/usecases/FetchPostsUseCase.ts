0af9df68ec07a3659ba01150609435f7b2e66ce2
alexSimch <alexey@xealenergy.com>
2025-07-25 11:09:42 -0400

    Change FetchPostsUseCase

--- app/domain/usecases/FetchPostsUseCase.ts
+++ app/domain/usecases/FetchPostsUseCase.ts
@@ -1,10 +1,13 @@
-import { postRepository } from "@/app/data/repositories/PostRepository";
+import { FeedRepository } from "@/app/data/repositories/FeedRepository";
+import { provideFeedRepository } from "@/app/data/repositories/ProvideFeedRepository";
 import { Post } from "../models/Post";
 
 export class FetchPostsUseCase {
+  constructor(private repository: FeedRepository) {}
+
   async execute(): Promise<Post[]> {
-    return postRepository.getPosts();
+    return this.repository.getPosts();
   }
 }
 
-export const fetchPostsUseCase = new FetchPostsUseCase();
+export const fetchPostsUseCase = new FetchPostsUseCase(provideFeedRepository());
